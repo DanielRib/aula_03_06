b54ee626f86a08e4d0b55ebbad4d02f45674b2f7
DanielRib <daniel.ppr97@hotmail.com>
2015-06-08 19:39:33 -0300

    mysqli

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+
+var_dump($_GET);
+phpinfo();
+//mysqli
+exit;
+
+?>
+
 <html>
     <head>
         <title>TODO supply a title</title>
